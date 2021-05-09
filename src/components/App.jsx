7fc4b4b22a2d85af09da52c625521e5dddd079c7
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-09 17:10:58 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import contacts from "../contacts";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card details={contacts[0]} />
+      <Card details={contacts[1]} />
+      <Card details={contacts[2]} />
+    </div>
+  );
+}
+
+export default App;
